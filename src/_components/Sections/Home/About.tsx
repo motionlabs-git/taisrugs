8b7ec28a671ef2dda84e2df0ed71002d1ccb3c1c
jindrahabarta <jindriskuv@email.cz>
2025-10-13 22:43:40 +0200

    about

---
+++ src/_components/Sections/Home/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <section className='h-[100dvh] w-full rounded-3xl bg-black  bg-radial-[at_80%80%] from-zinc-950 to-zinc-900 to-75%'>
+            About
+        </section>
+    )
+}
+
+export default About
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Hero from '@/_components/Sections/Hero'
+import About from '@/_components/Sections/Home/About'
+import Hero from '@/_components/Sections/Home/Hero'
 
 export default function Home() {
     return (
-        <div className=''>
+        <div className='w-full'>
             <Hero></Hero>
+
+            <About></About>
         </div>
     )
 }
