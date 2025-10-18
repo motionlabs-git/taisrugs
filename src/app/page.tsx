2a951e913e06659a4f96425c6fa2316a383d509d
lubahabarta <habarta@deepvision.cz>
2025-10-18 11:18:59 +0200

    added Motion component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Hero from '@/components/Sections/Home/Hero'
 import { getFavoriteProducts } from '@/utils/shopify/products'
 
 export default async function Home() {
-    const products = await getFavoriteProducts()
-    console.log(products)
+    // const products = await getFavoriteProducts()
+    // console.log(products)
 
     return (
         <div className='w-full'>
--- src/components/Sections/Home/Hero.tsx
+++ src/components/Sections/Home/Hero.tsx
@@ -1,9 +1,10 @@
+import MouseShift from '@/components/Motion/MouseShift'
 import React from 'react'
 
 const Hero = () => {
     return (
-        <div className='h-[100dvh] flex justify-center items-center'>
-            <div className=''>
+        <div className='relative h-[100dvh] flex justify-center items-center'>
+            <div className='absolute z-10'>
                 <h1 className='relative font-daffiys text-6xl text-black'>
                     Transform your space
                     <span className='absolute top-0 left-0 -translate-0.5 text-primary'>
@@ -11,6 +12,29 @@ const Hero = () => {
                     </span>
                 </h1>
             </div>
+
+            <MouseShift
+                scale={2}
+                className='w-full h-full absolute z-0 inset-0'
+            >
+                <div className='relative w-full h-full'>
+                    <div className='absolute right-3/5 top-3/5 w-[15%] aspect-square bg-blue-500' />
+
+                    <div className='absolute left-5/6 top-1/6 w-[20%] aspect-video bg-green-500' />
+
+                    <div className='absolute left-4/6 top-4/6 w-[14%] aspect-video bg-orange-500' />
+
+                    <div className='absolute left-2/6 top-1/5 w-[17%] aspect-video bg-red-500' />
+
+                    <div className='absolute left-1/2 top-1/2 w-[18%] aspect-video bg-purple-500' />
+
+                    <div className='absolute left-0 top-0 w-[18%] aspect-video bg-blue-500' />
+
+                    <div className='absolute right-0 bottom-0 w-[15%] aspect-video bg-green-500' />
+
+                    <div className='absolute left-0 bottom-0 w-[23%] aspect-square bg-red-500' />
+                </div>
+            </MouseShift>
         </div>
     )
 }
