076b059a51ff65b982b63463e2eb34c6eabccfd2
jindrahabarta <jindriskuv@email.cz>
2025-11-17 10:21:01 +0100

    HERO

--- src/app/(front)/components/Sections/Home/Hero.tsx
+++ src/app/(front)/components/Sections/Home/Hero.tsx
@@ -1,61 +1,22 @@
 import MouseShift from '@/app/(front)/components/Motion/MouseShift'
 import React from 'react'
 import Image from 'next/image'
+import WiggleButton from '../../Inputs/WiggleButton'
 
 const Hero = () => {
     return (
         <div className='relative w-full h-[100dvh] flex justify-center items-center'>
-            <div className='absolute'>
-                {/* <h1 className='group relative font-superVibes text-[4vw] text-primary font-bold'>
-                        <span className='absolute top-0 left-0 translate-y-2 -translate-x-2 text-violet-400 group-hover:translate-y-3 group-hover:-translate-x-3 duration-200'>
-                            Tais Rugsěšččšřáú
-                        </span>
-                        <span className='absolute top-0 left-0 translate-y-1 -translate-x-1 text-green-400 group-hover:translate-y-2 group-hover:-translate-x-2 duration-200'>
-                            Tais Rugsěšččšřáú
-                        </span>
-                        <span className='absolute top-0 left-0 text-primary group-hover:translate-y-1 group-hover:-translate-x-1 duration-200'>
-                            Tais Rugsěšččšřáú
-                        </span>
-                        <span className='relative text-black'>
-                            Tais Rugsěšččšřáú
-                        </span>
-                    </h1> */}
-
-                <h1 className='!text-[10vw] md:!text-[8vw] font-bold font-archivo max-w-[66vw] relative pl-[0.06em] pb-[0.06em] pt-[0.03em] pr-[0.03em] sm:hover:pt-[0.06em] sm:hover:pl-[0.03em] sm:hover:pr-[0.06em] sm:hover:pb-[0.03em] duration-200'>
-                    <span className='absolute inset-0 translate-y-[0.8vw] text-primary'>
-                        Transform your space
-                    </span>
-                    <span className='relative text-black'>
-                        Transform your space
-                    </span>
-                </h1>
-                <h1 className='group relative font-superVibes text-[7vw] text-primary font-bold'>
-                    <span className='absolute top-0 left-0 translate-y-2 -translate-x-2 text-violet-400 group-hover:translate-y-3 group-hover:-translate-x-3 duration-200'>
-                        Transform your space
-                    </span>
-                    <span className='absolute top-0 left-0 translate-y-1 -translate-x-1 text-green-400 group-hover:translate-y-2 group-hover:-translate-x-2 duration-200'>
-                        Transform your space
-                    </span>
-                    <span className='absolute top-0 left-0 text-blue-400 group-hover:translate-y-1 group-hover:-translate-x-1 duration-200'>
-                        Transform your space
-                    </span>
-                    <span className='relative text-black'>
-                        Transform your space
-                    </span>
-                </h1>
-            </div>
-
             <MouseShift
                 scale={2}
                 className='w-full h-full absolute z-0 inset-0'
             >
                 <div className='relative w-full h-full'>
-                    <div className='absolute right-3/5 top-3/5 w-[15%] aspect-square bg-blue-500 rounded-4xl hover:scale-105 duration-300'>
+                    <div className='absolute right-3/5 top-3/5 w-[10%] aspect-square hover:scale-105 duration-300'>
                         <Image
-                            src={'/images/Hero/1.png'}
+                            src={'/LogoSVG.svg'}
                             alt={'Hero image'}
-                            width={800}
-                            height={800}
+                            width={500}
+                            height={500}
                             className='w-full h-full object-cover'
                         />
                     </div>
@@ -123,6 +84,34 @@ const Hero = () => {
                     <div className='absolute left-0 bottom-0 w-[23%] aspect-squa rounded-4xl hover:scale-105 duration-300'></div>
                 </div>
             </MouseShift>
+
+            <div className='absolute'>
+                <div className='p-4 sm:max-w-[70vw]'>
+                    <h1 className='!text-[9vw] md:!text-[5vw] font-bold font-archivo mix-blend-difference relative pl-[0.06em] pb-[0.06em] sm:max-w-4/5 pt-[0.03em] pr-[0.03em] sm:hover:pt-[0.06em] sm:hover:pl-[0.03em] sm:hover:pr-[0.06em] sm:hover:pb-[0.03em] duration-200'>
+                        <span className='absolute inset-0 translate-y-[0.8vw] text-[#00247a]'>
+                            Transform your space today.
+                        </span>
+                        <span className='relative text-white'>
+                            Transform your space today.
+                        </span>
+                    </h1>
+
+                    <p className='mt-4 max-w-2/3 text-white mix-blend-difference'>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Voluptatum, totam accusamus velit officiis
+                        consequatur cumque maxime in quis inventore quas
+                        voluptates explicabo incidunt aspernatur eum molestiae?
+                        Mollitia magni temporibus vel.
+                    </p>
+
+                    <WiggleButton
+                        text={'Číst více'}
+                        link={'#about'}
+                        className='mt-8 invert  mix-blend-difference text-black '
+                        scrollTo
+                    />
+                </div>
+            </div>
         </div>
     )
 }
